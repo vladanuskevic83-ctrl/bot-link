3bd492c59261b53235337d6675efebfd858f85d1
vladanuskevic83-ctrl <vladanuskevic83@gmail.com>
2026-04-01 20:22:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,9 +36,7 @@ class LinkModal(ui.Modal, title="Paste Your Roblox Link"):
     async def shorten_url_isgd(self, session, original_url):
         try:
             headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.9',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Origin': 'https://is.gd',
                 'Referer': 'https://is.gd/',
